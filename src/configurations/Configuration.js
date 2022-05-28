8fe208716d1d249e4437ac978c2acb2b5e521516
Kakul Sahu <kakulsahu1511@outlook.com>
2022-05-29 00:45:06 +0530

    Updates

--- src/configurations/Configuration.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    Login: 'https://kakulengagefaceauthenticatorv1.azurewebsites.net/api/auth/signin',
-    SignUp: 'https://kakulengagefaceauthenticatorv1.azurewebsites.net//api/auth/signup',
-    Register: 'https://kakulengagefaceauthenticatorv1.azurewebsites.net//api/auth/upload'
-}
--- src/services/AxiosService.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios')
-
-export default class AxiosService {
-  post(url, data, IsRequired, Header) {
-    return axios.post(url, data, IsRequired && Header)
-  }
-}
